60c79fbf52c39bb705f11744794188644fdff6f7
cuongvd-1287 <van.dang.cuong@sun-asterisk.com>
2023-09-07 11:51:19 +0700

    a

--- app/src/main/java/com/example/androidTemplate/MainActivity.kt
+++ app/src/main/java/com/example/androidTemplate/MainActivity.kt
@@ -1,13 +1,11 @@
-package com.example.android_template
+package com.example.androidTemplate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-
 }
